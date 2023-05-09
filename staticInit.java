42ebb50b8009546cd6524a945f7bdc0cce0166f3
Ratul Mahmud <ratulmahmud18@gmail.com>
2023-05-09 17:15:22 +0200

    New file

---
+++ staticInit.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class staticInit {
+
+
+
+
+
+static int B;
+    static int H;
+    static boolean flag;
+
+    static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        scanner.close();
+
+        flag = B > 0 && H > 0;
+        if (!flag) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+} 
